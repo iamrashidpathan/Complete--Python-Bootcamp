e2ba0872ea8673c8f2edc736f5fb6ab8b987898e
Rashid Reyaz Khan <khanrashid11a@gmail.com>
2020-07-06 16:14:32 +0530

    Data Structure and objects

---
+++ Python Object and Data Structure Basics/Numbers.py
@@ -0,0 +1,30 @@
+# Arithmetic :
+
+# Addition
+print(2+1)
+
+# Subtraction
+print(2-1)
+
+# Multiplication
+print(2*3)
+
+# Division
+print(3/2)
+
+# Floor Division
+print(7 // 4)
+
+# Modulo
+print(7 % 4)
+
+# Power
+print(2 ** 3)
+
+# Order of operation
+print(2 + 10 * 10 +3)
+
+# Can use parentheses to specify orders
+print((2+10) * (10+3))
+
+
