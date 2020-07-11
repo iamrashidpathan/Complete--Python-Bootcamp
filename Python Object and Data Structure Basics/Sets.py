5a69347c52830e9510fb58b12a11bef0911f40ce
Rashid Reyaz Khan <khanrashid11a@gmail.com>
2020-07-11 20:58:39 +0530

    adding sets

---
+++ Python Object and Data Structure Basics/Sets.py
@@ -0,0 +1,12 @@
+my_set = set()
+my_set.add(1)
+print(my_set)
+my_set.add(2)
+print(my_set)
+my_set.add(2)
+print(my_set)
+
+# casting a list to set
+my_list = [1, 1, 1, 3, 2, 2, 3]
+my_set = set(my_list)
+print(my_set)
