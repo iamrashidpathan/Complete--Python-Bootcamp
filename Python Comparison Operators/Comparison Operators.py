cba907a996dcbd3e6d51a0dd1f2a7e0122f467b6
Rashid Reyaz Khan <khanrashid11a@gmail.com>
2020-08-25 12:59:49 +0530

    adding comparison operators

---
+++ Python Comparison Operators/Comparison Operators.py
@@ -0,0 +1,19 @@
+# Comparison operators return boolean values.
+
+print(2 == 2)  # returns true
+print(2 == 1)  # returns false
+
+print('hello' == 'Hello')  # comparing strings are size sensitive
+print('Hello' == "Hello")
+
+print('2' == 2)  # returns false
+print(1.0 == 1)  # returns true
+
+print(3 != 3)  # returns false
+print(4 != 5)  # returns true
+
+print(1 < 2)  # returns true
+print(1 > 2)  # returns false
+
+print(2 >= 2)  # returns true
+print(4 <= 37)  # returns true
